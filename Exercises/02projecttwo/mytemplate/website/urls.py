1c54b50d6b46e4fe2e3b2e71739bb744e27688c6
Josh Wood <wood2jp2@gmail.com>
2019-04-07 19:27:26 -0400

    first commit, starting with the end of section 3

---
+++ Exercises/02projecttwo/mytemplate/website/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
+    path('contact/', views.ContactUsPageView.as_view(), name='contact'),
+    
+]
---
+++ Exercises/02projecttwo/mytemplate/website/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomePageView(TemplateView): 
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class ContactUsPageView(TemplateView):
+    template_name = 'contact.html'
