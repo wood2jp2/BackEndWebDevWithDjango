2ba1c1bcfba00aae1fbda29abf900a5175f763a3
Josh Wood <wood2jp2@gmail.com>
2019-04-08 18:53:13 -0400

    created superuser, incorporated static files into templates, added their URLs, and created their view classes.

---
+++ Exercises/05projectfive/mywebsite/videorequest/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Video(models.Model):
+    videoTitle = models.CharField(max_length=20)
+    videoDescription = models.TextField()
+    dateAdded = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return 'Name: {}, ID: {}'.format(self.videoTitle, self.id)
