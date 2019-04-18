6f7e3741913afa3858dc7951dc9f0da3ba9bc49a
Josh Wood <wood2jp2@gmail.com>
2019-04-17 22:27:11 -0400

    connected our video requests home page with our database to render db videos

--- Exercises/05projectfive/mywebsite/videorequest/models.py
+++ Exercises/05projectfive/mywebsite/videorequest/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Video(models.Model):
-    videoTitle = models.CharField(max_length=20)
+    videoTitle = models.CharField(max_length=40)
     videoDescription = models.TextField()
     dateAdded = models.DateTimeField(default=timezone.now)
 
--- Exercises/05projectfive/mywebsite/videorequest/views.py
+++ Exercises/05projectfive/mywebsite/videorequest/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Video
 
 # Create your views here.
 def index(request):
-    return render(request, 'videorequest/index.html')
+    videos = Video.objects.order_by('-dateAdded')
+    context = {'videos': videos}
+    return render(request, 'videorequest/index.html', context)
 
 def vrform(request):
     return render(request, 'videorequest/vrform.html')
