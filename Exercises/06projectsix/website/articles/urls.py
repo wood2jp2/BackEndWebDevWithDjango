fd835d93fcd01e0f7ca4cb7981ebc3514396b044
Josh Wood <wood2jp2@gmail.com>
2019-04-25 11:01:06 -0400

    finished project 6 which had CRUD functionality.

--- Exercises/06projectsix/website/articles/urls.py
+++ Exercises/06projectsix/website/articles/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='home'),
     path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article'),
-    path('article/new/', views.ArticleCreateView.as_view(), name='article_new')
+    path('article/new/', views.ArticleCreateView.as_view(), name='article_new'),
+    path('article/<int:pk>/edit', views.ArticleEditView.as_view(), name='article_edit'),
+    path('article/<int:pk>/delete', views.ArticleDeleteView.as_view(), name='article_delete')
 ]
--- Exercises/06projectsix/website/articles/views.py
+++ Exercises/06projectsix/website/articles/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Article
 
 # Create your views here.
@@ -10,9 +11,22 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'detail.html'
+
+    # this name articles can be used to access the dbContext for whatever item is passed, in the template.
     context_object_name = 'articles'
 
 class ArticleCreateView(CreateView):
     model = Article
     template_name = 'article_new.html'
     fields = '__all__'
+
+class ArticleEditView(UpdateView):
+    model = Article
+    template_name = 'article_edit.html'
+    fields = ['title', 'text']
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    context_object_name = 'article'
+    success_url = reverse_lazy('home')
