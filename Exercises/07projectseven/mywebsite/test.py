ee09739765cab1c83477a05250b1f5cb0cf81e4c
Josh Wood <wood2jp2@gmail.com>
2019-05-06 21:09:14 -0400

    started project 8, including a login and logout feature.

--- Exercises/07projectseven/mywebsite/test.py
+++ Exercises/07projectseven/mywebsite/test.py
@@ -1,16 +1,68 @@
-def my_decorator(func):
-    def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
-    return wrapper
+# /* 
+# You are arranging images on the home page of a major website! You have a list of images in this format:
 
-@my_decorator
-def say_whee():
-    print("Whee!")
+#   [{'width': 300, 'height': 300, 'src': 'puppy.jpg'},
+#    {'width': 200, 'height': 500, 'src': 'kitty.png'}]
 
-@my_decorator
-def say_see():
-    print('see')
+# Your job is to organize these images into spaced rows. Given a page width and row height, scale each image so that it is exactly as tall as the row, and maintains its aspect ratio.
 
-say_see()
+# Then add each image into an array of rows, such that the total row width does not exceed the given page width. If an image can't fit within a given row, it should be added to the next row instead. You won't need to re-order the images.
+
+# In ascii art (not to scale) this would look like:
+
+# Before:
+# ------------------------
+# |                      |         -----------------------------      
+# |        kitty1        |---------|                           |
+# |       (100x80)       ||kitty2 ||        kitty3             |
+# |                      ||(10x20)||        (150x60)           | <more kitty photos>
+# --------------------------------------------------------------
+
+# After:
+# --------------------------------------------
+# |   kitty1    ||kitty2 ||      kitty3      |      <--- row 1
+# |  (50x40)    ||(20x40)||     (100x40)     |
+# ----------------------------------------------
+# |    kitty4      ||   kitty5       |  kitty6  |   <--- row 2
+# |   (80x40)      ||   (80x40)      |  (60x40) |
+# -----------------------------------------------
+
+
+# Your function should have the following signature:
+
+#   makeIntoRows(images, pageWidth, rowHeight)
+
+# See the following an example call and correct results for a page width of 220, and a row height of 40.
+
+# const images = [
+#   {'width': 100, 'height': 80, 'src': 'kitty1.jpg'},
+#   {'width': 10,  'height': 20, 'src': 'kitty2.jpg'},
+#   {'width': 150, 'height': 60, 'src': 'kitty3.jpg'},
+#   {'width': 10,  'height': 5, 'src': 'kitty4.jpg'},
+#   {'width': 40,  'height': 20, 'src': 'kitty5.jpg'},
+#   {'width': 60,  'height': 40, 'src': 'kitty6.jpg'}]
+
+# makeIntoRows(images, 220, 40)
+# [
+#   [
+    
+#     {'width': 50,  'height': 40, 'src': 'kitty1.jpg'},
+#     {'width': 20,  'height': 40, 'src': 'kitty2.jpg'},
+#     {'width': 100, 'height': 40, 'src': 'kitty3.jpg'}
+#   ], [
+#     {'width': 80,  'height': 40, 'src': 'kitty4.jpg'},
+#     {'width': 80,  'height': 40, 'src': 'kitty5.jpg'},
+#     {'width': 60,  'height': 40, 'src': 'kitty6.jpg'}
+#   ]
+# ]
+
+# You can assume:
+# - Each image has positive integer height and widths.
+# - No image is wider than the given page width.
+
+# For a refresher on ratios: x1/y1 = x2/y2
+# - The ratio between two images is:
+#   resizedImageWidth/resizedImageHeight = origImageWidth/origImageHeight
+# - solving for resizedImageWidth:
+#   resizedImageWidth = origImageWidth/origImageHeight * resizedImageHeight
+#  */
